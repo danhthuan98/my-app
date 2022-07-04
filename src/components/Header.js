633af12e618841805b515c919e55b4cc55543284
danhthuan98 <danhthuan98bn@gmail.com>
2022-07-05 01:30:28 +0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// import { FaLaptop, FaTabletAlt, FaMobileAlt } from 'react-icons/fa';
+// import useWindowSize from './hooks/useWindowSize';
+
+const Header = ({ title }) => {
+
+    return (
+        <header className="Header">
+            <h1>{title}</h1>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+
+const Home = () => {
+
+    return (
+        <main className="Home">
+            This is Home Page
+        </main>
+    )
+}
+
+export default Home
